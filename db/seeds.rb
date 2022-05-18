4a33f45a95cd0427550e414743b1570f4a8eb95c
Claudia <claudia.araya.ro@gmail.com>
2022-05-18 17:23:05 -0400

    cambio en seeds para mostrar imágenes

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,8 @@ require 'faker'
         description: Faker::Marketing.buzzwords,
         price: Faker::Commerce.price,
         category: Faker::Commerce.material,
-        image_url: Faker::Avatar.image
+        image_url: "https://loremflickr.com/#{rand(150..200)}/#{rand(150..200)}/all"
+        #image_url: Faker::LoremFlickr.image
         #image_url: Faker::Avatar.image(size: "50x50")
     )
 end
